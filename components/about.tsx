6f51f5d5201fa8193076566e6fb1e4fc2fd39061
vipul <vipul.devs99@gmail.com>
2024-02-27 23:41:31 +0530

    added

---
+++ components/about.tsx
@@ -0,0 +1,57 @@
+// client-side component
+"use client";
+
+// Import necessary dependencies and components.
+import { motion } from "framer-motion";
+
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
+
+// Define the About component.
+const About = () => {
+  // Use the useSectionInView custom hook to track when the "About" section is in view.
+  const { ref } = useSectionInView("About");
+
+  // Return the About section, which uses framer-motion for animations.
+  return (
+    <motion.section
+      ref={ref}
+      id="about"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+    >
+      {/* Display the section heading for "About me." */}
+      <SectionHeading>About me</SectionHeading>
+
+      {/* Display a paragraph with information about the user's background and interests. */}
+      <p className="mb-3">
+        After passing out my <b className="font-medium">High School</b>, I
+        decided to pursue my passion for programming. I enrolled in a coding
+        bootcamp and learned{" "}
+        <b className="font-medium">software developer web development</b>.{" "}
+        <i className="italic">My favorite part of programming</i> is the
+        problem-solving aspect. I <u className="underline">love</u> the feeling
+        of finally figuring out a solution to a problem. My core stack is{" "}
+        <b className="font-medium">React, Next.js, Node.js, and MongoDB</b>. I
+        am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <b className="font-medium">full-time position</b> as a software
+        developer.
+      </p>
+
+      {/* Display another paragraph about the user's interests and hobbies. */}
+      <p>
+        <i className="italic">When I'm not coding</i>, I enjoy playing video
+        games, watching movies, and playing with my dog. I also enjoy{" "}
+        <b className="font-medium">learning new things</b>. I am currently
+        learning about <b className="font-medium">history and philosophy</b>.
+        I'm also learning how to play the guitar.
+      </p>
+    </motion.section>
+  );
+};
+
+// Export the About component.
+export default About;
---
+++ constants/index.ts
@@ -0,0 +1,120 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+
+import carhubImg from "@/public/carhub.png";
+import realtorImg from "@/public/realtor.png";
+import ecommerceImg from "@/public/ecommerce.png";
+
+// Navigation links
+export const LINKS = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+// External links
+export const EXTRA_LINKS = {
+  linkedin: "https://www.linkedin.com/in/vipul-maurya",
+  github: "https://github.com/vipul-it",
+  resume: "/resume.pdf",
+  source_code: "https://github.com/vipul-it",
+  email: "Vipul.devs99@gmail.com",
+} as const;
+
+// Data for work experience
+export const EXPERIENCES_DATA = [
+  {
+    title: "Passed High School",
+    location: "Sitapur, IN",
+    description:
+      "Completed my high school curriculum, gaining a solid foundation in various subjects and essential skills.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021",
+  },
+  {
+    title: "React.js Bootcamp",
+    location: "Lucknow, IN",
+    description:
+      "Successfully completed an intensive 1-month React.js Developer Bootcamp. Acquired expertise in React components, state management with Redux, and interacting with RESTful APIs.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022",
+  },
+  {
+    title: "Freelancer React.js Developer",
+    location: "Bangalore, IN",
+    description:
+      "Working as a freelance React.js Developer, I have collaborated with various clients to create responsive and dynamic web applications.",
+    icon: React.createElement(FaReact),
+    date: "2022 - present",
+  },
+] as const;
+
+// Data for projects
+export const PROJECTS_DATA = [
+  {
+    title: "CarHub",
+    description:
+      "A web app that is built using NextJS and enables users to quickly search and obtain information about cars.",
+    tags: ["React", "Next.js", "TypeScript", "Tailwind", "Headless UI"],
+    imageUrl: carhubImg,
+    projectUrl: "https://carhb.vercel.app/",
+  },
+  {
+    title: "Realtor",
+    description:
+      "Buy and rent homes for everyone. I was the front-end developer. It has features like filtering, sorting, and pagination.",
+    tags: ["React", "Chakra UI", "Next.js", "Framer Motion", "React Icons"],
+    imageUrl: realtorImg,
+    projectUrl: "https://real-estate-app-react.vercel.app/",
+  },
+  {
+    title: "ECommerce Store",
+    description:
+      "A NextJS-based eCommerce store that allows users to order different products. It supports real-time payments using Stripe.",
+    tags: ["React", "Next.js", "Sanity", "React Router", "Stripe"],
+    imageUrl: ecommerceImg,
+    projectUrl: "https://ecommerce-app-next.vercel.app/",
+  },
+] as const;
+
+// Data for skills
+export const SKILLS_DATA = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Redux",
+  "Framer Motion",
+] as const;
+
+// Owner name
+export const OWNER_NAME = "Vipul Maurya";
